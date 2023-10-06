1a0b8293360f01d1a3b86aa336fab4aa17c7940d
RenanLucas19 <renanlucas186@gmail.com>
2023-10-06 11:40:43 -0300

    Mais alterações

---
+++ Guia turistico/src/ENTIDADES/Usuarios.java
@@ -0,0 +1,33 @@
+package ENTIDADES;
+
+
+public class Usuarios {
+    private String email;
+    private String senha;
+    
+
+    public Usuarios( String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    
+    }
+
+    // Métodos getters e setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
---
+++ Guia turistico/src/GUIA/Usuario.java
@@ -0,0 +1,77 @@
+package GUIA;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import ENTIDADES.Usuarios;
+
+public class Usuario{
+
+public void cadastrarLogin(ArrayList <Usuarios> cadastrarLogin ) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Informe seu Email: ");
+        String email = ler.nextLine();
+        System.out.print("Crie uma Senha: ");
+        String senha = ler.nextLine();
+
+        Usuarios cadastraLogin = new Usuarios(email, senha);
+        cadastrarLogin.add(cadastraLogin);
+        
+        System.out.println("Cadastro realizado com sucesso!\n");
+
+        ler.close();
+
+    }
+
+    public void editarCadastro(ArrayList <Usuarios> UsuarioNovo) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Cadastro de Usuário");
+        System.out.print("Informe seu Email: ");
+        String novoEmail = ler.nextLine();
+
+        System.out.print("Crie uma Senha: ");
+        String novaSenha = ler.nextLine();
+
+        Usuarios novoUsuario = new Usuarios(novoEmail, novaSenha);
+        UsuarioNovo.add(novoUsuario);
+
+        System.out.println("Cadastro realizado com sucesso!\n");
+
+    
+        System.out.println("Editando Cadastro de Usuário...");
+        editarCadastro(UsuarioNovo);
+
+        System.out.println("Cadastro de Usuário atualizado com sucesso!");
+
+        ler.close();
+    }
+
+    public void ExcluiCadastro(ArrayList <Usuarios> cadastraLogin){
+    Scanner ler = new Scanner(System.in);
+    System.out.print("Informe o Email: ");
+    String email = ler.nextLine();
+    System.out.print("Informe a Senha: ");
+    String senha = ler.nextLine();
+
+    // Verificar se o login e a senha correspondem a um usuário na lista
+    Usuarios usuarioParaExcluir = null;
+
+    for (Usuarios usuario : cadastraLogin) {
+        if (usuario.getEmail().equals(email) && usuario.getSenha().equals(senha)) {
+            usuarioParaExcluir = usuario;
+            break; // Encontrou o usuário, podemos sair do loop
+        }
+    }
+
+    if (usuarioParaExcluir != null) {
+        // Remover o usuário da lista
+        cadastraLogin.remove(usuarioParaExcluir);
+        System.out.println("Cadastro de Usuário excluído com sucesso!");
+    } else {
+        System.out.println("Credenciais incorretas. Não foi possível excluir o cadastro.");
+    }
+
+    ler.close();
+    }
+}
