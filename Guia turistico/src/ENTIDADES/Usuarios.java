f3b8ffd27d3924927a7e534463d32142ae061436
AmanditaC <amandacristinagomes8@gamail.com>
2023-10-22 10:54:51 -0300

    Modificações em Usuarios

--- Guia turistico/src/ENTIDADES/Usuarios.java
+++ Guia turistico/src/ENTIDADES/Usuarios.java
@@ -5,35 +5,30 @@ public class Usuarios {
     private String email;
     private String senha;
     
-
-    public Usuarios( String email, String senha) {
+    public Usuarios(String email, String senha) {
         this.email = email;
         this.senha = senha;
-    
     }
 
+     // Métodos getters e setters
 
     public String getEmail() {
         return email;
     }
 
-
     public void setEmail(String email) {
-        email = email;
+        this.email = email;
     }
-
 
     public String getSenha() {
         return senha;
     }
 
-
     public void setSenha(String senha) {
-        senha = senha;
+        this.senha = senha;
     }
 
-    // Métodos getters e setters
-
+    //Método toString
     public String toString(){
         
         return "Email: " + email +
